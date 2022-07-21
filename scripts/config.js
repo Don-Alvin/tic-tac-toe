49e837892e3d9138c5ab8806c5cf3b38f8b88b86
Don-Alvin <alvindon41@gmail.com>
2022-07-21 20:46:02 +0300

    More Javascript

--- scripts/config.js
+++ scripts/config.js
@@ -10,6 +10,7 @@ function closePlayerConfig() {
 	backdropElement.style.display = "none";
 	formElement.firstElementChild.classList.remove("error");
 	errorOutput.textContent = "";
+	formElement.firstElementChild.lastElementChild.value = "";
 }
 
 function savePlayerConfig(event) {
@@ -22,4 +23,17 @@ function savePlayerConfig(event) {
 		errorOutput.textContent = "Please enter a valid name!";
 		return;
 	}
+
+	const updatedPlayerDataElement = document.getElementById(
+		"player-" + editedPlayer + "-data"
+	);
+	updatedPlayerDataElement.children[1].textContent = enteredUsername;
+
+	if (editedPlayer === 1) {
+		players[0].name = enteredUsername;
+	} else {
+		players[1].name = enteredUsername;
+	}
+
+	closePlayerConfig();
 }
